1d2adfc42306b4b2a703d3936e6cee860ec21cf0
James Confidence <63427518+jamesconfy@users.noreply.github.com>
2023-05-05 14:56:57 +0100

    Cleaned structure

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -6,7 +6,7 @@ import (
 )
 
 func NewLogger() *os.File {
-	f, err := os.OpenFile("./logs/gin.log", os.O_RDWR, 0666)
+	f, err := os.OpenFile("./logs/gin.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
 
 	if err != nil {
 		log.Panic(err)
